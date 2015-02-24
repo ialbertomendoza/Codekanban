b26ef981cd2d2a8e2cb2009548e798eb33cf5b0e
TRON <osvaldocube@gmail.com>
2015-02-24 15:26:28 -0600

    Agregado comportamiento a tareas.

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -1,11 +1,8 @@
 'use strict';
 function asignaTarea() {
-  //var id = $(this).data('id');
   var estado = $(this).parent().attr('id');
   $(this).find('.tarea').removeClass('box-pendiente').removeClass('box-asignado').removeClass('box-desarrollo').removeClass('box-pruebas').removeClass('box-lanzamiento');
   $(this).find('.tarea').addClass('box-'+estado);
-  //console.log(estado);
-  //console.log(id);
 }
 
 function peticionApi(metodo,url,parametros) {
@@ -17,10 +14,6 @@ function peticionApi(metodo,url,parametros) {
   if((typeof parametros != 'undefined') || (parametros != false)){
     opcionesAjax.data = parametros;
   };
-
-  //console.log("Peticion AJAX: "+metodo);
-  //console.log("Peticion url: "+url);
-  //console.log("Peticion parametros: "+parametros);
   return $.ajax(opcionesAjax);
 }
 
@@ -29,22 +22,28 @@ function inputClear(){
   $('.modal').modal('hide');
 }
 
+$(document).ready(function(){
+  proyecto.query.list();
 
-$('#pendiente, #asignado, #desarrollo, #pruebas, #lanzamiento').dragsort({ dragSelector: '.tarea', dragBetween: true, dragEnd: asignaTarea, placeHolderTemplate: '<li class="placeHolder"><div></div></li>' });
+  $('#pendiente, #asignado, #desarrollo, #pruebas, #lanzamiento').dragsort({ dragSelector: '.tarea', dragBetween: true, dragEnd: asignaTarea, placeHolderTemplate: '<li class="placeHolder"><div></div></li>' });
 
-$('.col-estado').on('click','li div', function(){
-  $('#modalTarea').modal('show');
+  $('.col-estado').on('click','li div', function(){
+    $('#modalTarea').modal('show');
+  });
+
+  $('#btn-task-new').on('click', function(){
+    $('#modalNuevaTarea').modal('show');
+    return false;
+  });
+
+  $('#btn-project-new').on('click', function(){
+    $('#modalNuevoProyecto').modal('show');
+    return false;
+  });
 });
 
-$('#btn-task-new').on('click', function(){
-  $('#modalNuevaTarea').modal('show');
-  return false;
-});
 
-$('#btn-project-new').on('click', function(){
-  $('#modalNuevoProyecto').modal('show');
-  return false;
-});
+
 
 
 
---
+++ frontend/app/scripts/proto.js
@@ -0,0 +1,47 @@
+var proyecto = {
+  query:{
+    new: function(data){
+      peticionApi('PUT',myApp.url.rest+'proyecto',data)
+        .done(function(dato){
+          inputClear();
+          console.log(dato);
+          console.log('Datos insertados');
+        })
+        .fail(function(){
+          console.log('Ha ocurrio un error');
+        });
+    },
+    list: function(){
+      peticionApi('GET',myApp.url.rest+'proyecto')
+        .done(proyecto.render.carga);
+    }
+  },
+  render:{
+    carga: function(data){
+      console.log(data);
+      $('.select-proyecto').html('');
+      $.each( data, function( i, item ) {
+        var html = '<option value="'+item.nombre+'">'+item.nombre+'</option>';
+        $(html).appendTo('.select-proyecto');
+      });
+
+    }
+  }
+};
+
+var tarea = {
+  query:{
+    new: function(data){
+      console.log(data);
+      peticionApi('PUT',myApp.url.rest+'tarea',data)
+        .done(function(dato){
+          inputClear();
+          console.log(dato);
+          console.log('Datos insertados');
+        })
+        .fail(function(){
+          console.log('Ha ocurrio un error');
+        });
+    }
+  }
+};
