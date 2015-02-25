16d805fec59e0c73f7e1e7df995d468c5838cb2e
DanielGandarillas <gandani.91@gmail.com>
2015-02-24 18:42:45 -0600

    emmit en socket creadoproyecto

--- backend/api/controllers/ProyectoController.js
+++ backend/api/controllers/ProyectoController.js
@@ -17,7 +17,9 @@ module.exports = {
 				console.log('entro al error');
 				return next(err);
 			}
-			Proyecto.publishCreate({"status":"true","response":proyecto});
+			var socket = req.socket;
+			var io = sails.io;
+			io.sockets.emit('nuevoProyecto', {"status":"true","response":proyecto});
 			return res.json(201, {"status":"true","response":{"id":proyecto.id}});
 		});
 	},
